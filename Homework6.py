90011c6f9ed196732ff26a76f57cf7b1cff0a763
aelieber1 <80122879+aelieber1@users.noreply.github.com>
2023-02-24 23:17:53 -0600

    updates to hw

--- Homework6.py
+++ Homework6.py
@@ -8,42 +8,110 @@ author: @aelieber1 + several sources cited throughout code
 # Import necessary functions
 import scipy.integrate as spi
 import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    # if the user includes the flag -h or --help print the options
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print ("Usage: %s [-seed number]" % sys.argv[0])
+        print (" -Nint: [integer # of sub intervals]")
+        print (" -upper: [upper limit of integration]")
+        print (" -lower: [lower limit of integration]")
+        sys.exit(1)
+
+    # default lower bound
+    a = 0
+    
+    # default upper bound
+    b = 10
+    
+    # default number of sub intervals
+    Nint = 1
+   
+    # read the user-provided input from the command line (if there)
+    if '-Nint' in sys.argv:
+        p = sys.argv.index('-Nint')
+        Nint = sys.argv[p+1]
+    if '-upper' in sys.argv:
+        p = sys.argv.index('-upper')
+        utemp = float(sys.argv[p+1])
+        if utemp > 0:
+            b = utemp
+    if '-lower' in sys.argv:
+        p = sys.argv.index('-lower')
+        ltemp = int(sys.argv[p+1])
+        if ltemp > 0:
+            a = ltemp
+
+    """ 
+    Function definition on a closed interval that is not a low order polynomial, but whose integral can still be calculated analytically 
+
+    Credit: 
+    - utilizes code from this integrals in python website: 
+    https://computationalmindset.com/en/mathematics/integral-calculus-in-python.html
+    - scipy.integrate documentation: 
+    https://docs.scipy.org/doc/scipy/tutorial/integrate.html
+    """
+    print(a)
+    print(b)
+    # Analytical calculation of integral
+    def f(x):
+        return 4 * x * np.exp(-x)
+
+    def f_prime(x):
+        return 4 * (-x - 1) * np.exp(-x)
+
+    # Evaluate integral
+    upper_calc = f_prime(b)
+    lower_calc = f_prime(a)
+    analyt_int = upper_calc - lower_calc 
+
+    print('Analytical Calculation of Integral of 4xe^-x from x=1 to x=10: ', analyt_int)
+
+    # plot function
+    x = np.linspace(0, 11, 100)
+    plt.plot(x, f(x), color='red')
+    #plt.show()
 
 
-""" 
-Function definition on a closed interval that is not a low order polynomial, but whose integral can still be calculated analytically 
+    """
+    Implement two different numerical integration methods to integrate this function (don't need to be of very high order)
+    1. fixed interval sizes
+    2. Gaussian quandrature
+    * write code such that you can change the number of sub intervals and number of evaluation points -- TODO: do cmd line input as earlier (check)
+    """
 
-Credit: 
-- utilizes code from this integrals in python website: 
-https://computationalmindset.com/en/mathematics/integral-calculus-in-python.html
-- scipy.integrate documentation: 
-https://docs.scipy.org/doc/scipy/tutorial/integrate.html
-"""
+    # calculation of the integral with fixed interval size
+    funct = lambda x: 4 * x * np.exp(-x)
+    # using scipy.integrate.romberg()
+    result_romb = spi.romberg(f, 0, 10, show = True)
 
-print('Integral of 4xe^-x from x=1 to x=5')
+    print("The numerical result using Romberg's method with evenly space sub-intervals is {:f} (+-{:g})"
+        .format(result_romb, a))
 
-# integral definition
-integrand = lambda x : 4 * x * np.exp(-x)
-a = 1. # lower bound
-b = 5. # upper bound
-
-# calculation of the integral analytically using scipy
-# utilizes scipy.quad which is general purpose integration in scipy literature
-result, error = spi.quad(integrand, a, b)
-print('Result is ', result, 'with error ', error)
-
-"""
-Implement two different numerical integration methods to integrate this function (don't need to be of very high order)
-1. fixed interval sizes
-2. Gaussian quandrature
-* write code such that you can cahnge the number of sub intervals and number of evaluation points -- TODO: do cmd line input as earlier
-"""
+    # calculation of integral using Gaussian quadrature
+    result_gaus, error_gaus = spi.quadrature(f, a, b)
+    print("The numerical result using Gaussian Quadrature is {:f} (+-{:g})"
+        .format(result_gaus, error_gaus))
 
 
-"""
-1. Compare the difference between the two numerical integrals
-2. Compare the difference with the correct/analytical answer
-* both as functions of the number of sub intervals
+    """
+    1. Compare the difference between the two numerical integrals
+    2. Compare the difference with the correct/analytical answer
+    * both as functions of the number of sub intervals
 
-Answer: Is the difference between the two estimates a good indicator of the actual error? 
-"""
+    Answer: Is the difference between the two estimates a good indicator of the actual error? 
+    """
+    
+    # Difference in the two numerical integrals
+    numerical_difference = abs(result_romb - result_gaus)
+    print('Difference betweent the two numerical integrals: ', numerical_difference)
+    
+    # Comparison of Numerical Integrals to the Correct Analytical Answer
+    def percent (t, u):
+        result = abs(float(((u - t) * 100) / t))
+        return result
+    
+    print("Percent difference between romb and correct integral: ", percent(result_romb, analyt_int), "%")
+    print("Percent difference between gaus quad and correct integral: ", percent(result_gaus, analyt_int), "%")
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # PHSX815_Week6
 
-## Choose a function on a closed interval (the function *cannot* be a low order polynomial, but need not be too complicated). Choose a function that you can calculate the integral for analytically.
+## Instructions: 
+- Choose a function on a closed interval (the function *cannot* be a low order polynomial, but need not be too complicated). Choose a function that you can calculate the integral for analytically.
 
-## Implement *two different* numerical integration methods, one with fixed interval sizes, one with Gaussian quadrature, to integrate this function. These don't need to be of very high order. You should write your code such that you can change the number of sub-intervals (and number of evaluation points).
+- Implement *two different* numerical integration methods, one with fixed interval sizes, one with Gaussian quadrature, to integrate this function. These don't need to be of very high order. You should write your code such that you can change the number of sub-intervals (and number of evaluation points).
 
-## Compare the difference between the two numerical integrals, and their difference with the correct/analytic answer, as a function of the number sub-intervals. Is the difference between the two estimates a good indicator of the actual error?
+- Compare the difference between the two numerical integrals, and their difference with the correct/analytic answer, as a function of the number sub-intervals. Is the difference between the two estimates a good indicator of the actual error?
+
+
+## How to Run
+The python file `Homework6.py`can be run from the command line by typing:
+
+	<> python3 Homework6.py -Nint [Number of Sub-Intervals] -upper [upper limit of integration] -lower [lower limit of integration] 
